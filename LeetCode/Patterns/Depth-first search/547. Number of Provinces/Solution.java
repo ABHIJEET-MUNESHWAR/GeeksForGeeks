7b21a13594f1e754edb255c2f619c29680d6ea05
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-21 18:32:23 +0530

    Add Java Solution to Patterns/Depth-first search

---
+++ LeetCode/Patterns/Depth-first search/547. Number of Provinces/Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public int find(int i, int[] parent) {
+        if (i == parent[i]) {
+            return i;
+        }
+        return parent[i] = find(parent[i], parent);
+    }
+
+    public void union(int i, int j, int[] parent, int[] rank) {
+        int parentOfI = find(i, parent);
+        int parentOfJ = find(j, parent);
+        if (parentOfI == parentOfJ) {
+            return;
+        }
+        if (rank[parentOfI] > rank[parentOfJ]) {
+            parent[parentOfJ] = parentOfI;
+        } else if (rank[parentOfI] < rank[parentOfJ]) {
+            parent[parentOfI] = parentOfJ;
+        } else {
+            parent[parentOfI] = parentOfJ;
+            rank[parentOfJ]++;
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    union(i, j, parent, rank);
+                }
+            }
+        }
+        int provinces = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+}
