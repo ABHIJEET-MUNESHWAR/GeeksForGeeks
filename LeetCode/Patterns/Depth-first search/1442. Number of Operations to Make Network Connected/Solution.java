b8baa0de463862a790f90ed054e4b210632e0330
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-21 20:41:46 +0530

    Add Java Solution to Patterns/Depth-first search

--- LeetCode/Patterns/Depth-first search/1442. Number of Operations to Make Network Connected/Solution.java
+++ LeetCode/Patterns/Depth-first search/1442. Number of Operations to Make Network Connected/Solution.java
@@ -1,7 +1,6 @@
 class Solution {
-
     public int find(int i, int[] parent) {
-        if (parent[i] == i) {
+        if (i == parent[i]) {
             return i;
         }
         return parent[i] = find(parent[i], parent);
@@ -13,31 +12,34 @@ class Solution {
         if (parentOfI == parentOfJ) {
             return;
         }
-        if (rank[parentOfI] > rank[parentOfJ]) {
+        if (rank[parentOfI] < rank[parentOfJ]) {
+            parent[parentOfI] = parentOfJ;
+        } else if (rank[parentOfI] > rank[parentOfJ]) {
             parent[parentOfJ] = parentOfI;
-        } else if (rank[parentOfI] < rank[parentOfJ]) {
-            parent[parentOfI] = parentOfJ;
         } else {
-            parent[parentOfI] = parentOfJ;
-            rank[parentOfJ]++;
+            parent[parentOfJ] = parentOfI;
+            rank[parentOfI]++;
         }
     }
 
     public int makeConnected(int n, int[][] connections) {
-        int size = connections.length;
-        if (size < (n - 1)) {
+        if (connections.length < n - 1) {
             return -1;
         }
-        int[] parents = new int[n + 1];
+        int[] parent = new int[n + 1];
         int[] rank = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            parents[i] = i;
+            parent[i] = i;
             rank[i] = 0;
         }
         int components = n;
         for (int[] connection : connections) {
-            if (find(connection[0], parents) != find(connection[1], parents)) {
-                union(connection[0], connection[1], parents, rank);
+            int i = connection[0];
+            int j = connection[1];
+            int parentOfI = find(i, parent);
+            int parentOfJ = find(j, parent);
+            if (parentOfI != parentOfJ) {
+                union(i, j, parent, rank);
                 components--;
             }
         }
