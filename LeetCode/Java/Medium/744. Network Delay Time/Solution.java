9549e09184992d92775d4db15d925a742d99c23f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-22 21:32:06 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/744. Network Delay Time/Solution.java
+++ LeetCode/Java/Medium/744. Network Delay Time/Solution.java
@@ -1,41 +1,40 @@
 class Solution {
-    public int networkDelayTime(int[][] times, int n, int k) {
-
-        // Step 1
-        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
-        for (int[] time : times) {
-            int start = time[0], end = time[1], weight = time[2];
-            graph.putIfAbsent(start, new HashMap<>());
-            graph.get(start).put(end, weight);
+    public int networkDelayTime(int[][] edges, int n, int src) {
+        Map<Integer, ArrayList<int[]>> adj = new HashMap<>();
+        for (int i = 0; i < n + 1; i++) {
+            adj.put(i, new ArrayList<>());
         }
-
-        // Step 2
-        int[] distances = new int[n + 1];
-        Arrays.fill(distances, Integer.MAX_VALUE);
-        distances[k] = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] { k, 0 });
-
-        // Step 3:
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int currentNode = current[0], currentWeight = current[1];
-            for (int neighbor : graph.getOrDefault(currentNode, new HashMap<>()).keySet()) {
-                int neighbourWeight = graph.get(currentNode).get(neighbor);
-                if (distances[neighbor] > currentWeight + neighbourWeight) {
-                    distances[neighbor] = currentWeight + neighbourWeight;
-                    queue.add(new int[] { neighbor, currentWeight + neighbourWeight });
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int weight = edge[2];
+            adj.get(u).add(new int[] { v, weight });
+        }
+        int[] result = new int[n + 1];
+        Arrays.fill(result, Integer.MAX_VALUE);
+        result[src] = 0;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        minHeap.offer(new int[] { 0, src });
+        while (!minHeap.isEmpty()) {
+            int[] current = minHeap.poll();
+            int u = current[1];
+            int currentDistance = current[0];
+            if (currentDistance > result[u]) {
+                continue;
+            }
+            for (int[] neighbour : adj.get(u)) {
+                int v = neighbour[0];
+                int weight = neighbour[1];
+                if (result[v] > (weight + result[u])) {
+                    result[v] = weight + result[u];
+                    minHeap.offer(new int[] { result[v], v });
                 }
             }
         }
-
-        // Step 4:
-        int result = 0;
+        int maxDistance = 0;
         for (int i = 1; i <= n; i++) {
-            if (result < distances[i]) {
-                result = Math.max(result, distances[i]);
-            }
+            maxDistance = Math.max(maxDistance, result[i]);
         }
-        return result == Integer.MAX_VALUE ? -1 : result;
+        return maxDistance == Integer.MAX_VALUE ? -1 : maxDistance;
     }
 }
