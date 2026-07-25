9a816a123c98f110044f069d30f8da9f06c38a49
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-25 13:51:57 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/1876. Map of Highest Peak/Solution.java
+++ LeetCode/Patterns/Arrays/1876. Map of Highest Peak/Solution.java
@@ -8,13 +8,13 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (isWater[i][j] == 1) {
                     height[i][j] = 0;
-                    queue.add(new int[] { i, j });
+                    queue.offer(new int[] { i, j });
                 } else {
                     height[i][j] = -1;
                 }
             }
         }
-        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+        int[][] directions = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
         while (!queue.isEmpty()) {
             int size = queue.size();
             while (size-- > 0) {
@@ -24,9 +24,9 @@ class Solution {
                 for (int[] dir : directions) {
                     int i_ = i + dir[0];
                     int j_ = j + dir[1];
-                    if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && height[i_][j_] == -1) {
+                    if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && height[i_][j_] == -1) {
                         height[i_][j_] = height[i][j] + 1;
-                        queue.add(new int[] { i_, j_ });
+                        queue.offer(new int[] { i_, j_ });
                     }
                 }
             }
