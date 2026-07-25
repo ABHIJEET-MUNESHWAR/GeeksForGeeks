0e637a0d31dfc6fdc511dfdf6ad60ea27fc86156
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-25 14:12:38 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/3859. Maximum Product of Two Digits/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProduct(int n) {
+
+        int first = 0;
+
+        int second = 0;
+
+        while (n > 0) {
+
+            int digit = n % 10;
+
+            if (digit >= first) {
+                second = first;
+                first = digit;
+            } else if (digit > second) {
+                second = digit;
+            }
+
+            n /= 10;
+        }
+
+        return first * second;
+    }
+}
