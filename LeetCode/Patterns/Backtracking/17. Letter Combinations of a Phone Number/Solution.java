5a27b6754658b8f45e508feb60430368d4426c71
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-29 21:36:43 +0530

    Add Java Solution to Patterns/Backtracking

--- LeetCode/Patterns/Backtracking/17. Letter Combinations of a Phone Number/Solution.java
+++ LeetCode/Patterns/Backtracking/17. Letter Combinations of a Phone Number/Solution.java
@@ -1,24 +1,24 @@
 class Solution {
-
-    List<String> result = new ArrayList<>();
-    String[] mapping = new String[] { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    List<String> combinations = new ArrayList<>();
+    String[] numToStrMap = { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) {
-            return result;
+            return combinations;
         }
         solve(digits, 0, "");
-        return result;
+        return combinations;
     }
 
-    private void solve(String digits, int index, String temp) {
+    public void solve(String digits, int index, String temp) {
         if (index >= digits.length()) {
-            result.add(temp);
+            combinations.add(temp);
             return;
         }
         char ch = digits.charAt(index);
-        String str = mapping[ch - '0'];
-        for (int i = 0; i < str.length(); i++) {
+        String str = numToStrMap[ch - '0'];
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
             String tmp = temp;
             temp += str.charAt(i); // Do
             solve(digits, index + 1, temp); // Explore
