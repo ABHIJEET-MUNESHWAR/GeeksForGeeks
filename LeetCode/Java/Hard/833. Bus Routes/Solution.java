5c88262320887ebed65083bd5975349f26d9b80d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 13:16:32 +0530

    Add Hard Java Solution from LeetCode

--- LeetCode/Java/Hard/833. Bus Routes/Solution.java
+++ LeetCode/Java/Hard/833. Bus Routes/Solution.java
@@ -1,47 +1,42 @@
 class Solution {
     public int numBusesToDestination(int[][] routes, int source, int target) {
-        
         if (source == target) {
             return 0;
         }
-        // Create a map from the bus stop to all the routes that include this stop.
-        Map<Integer, List<Integer>> busRouteToStopIndexMap = new HashMap<>();
-        int n = routes.length;
-        for (int i = 0; i < n; i++) {
-            int[] route = routes[i];
-            int m = route.length;
-            for (int j = 0; j < m; j++) {
-                busRouteToStopIndexMap.computeIfAbsent(route[j], k -> new ArrayList<>()).add(i);
+        Map<Integer, List<Integer>> busStopToRouteIndexMap = new HashMap<>();
+
+        int noOfRoutes = routes.length;
+        for (int i = 0; i < noOfRoutes; i++) {
+            int[] busStops = routes[i];
+            int busStopsLength = busStops.length;
+            for (int j = 0; j < busStopsLength; j++) {
+                busStopToRouteIndexMap.computeIfAbsent(busStops[j], key -> new ArrayList<>()).add(i);
             }
         }
         Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[501];
-        // Insert all the routes in the queue that have the source stop.
-        for (int route : busRouteToStopIndexMap.getOrDefault(source, new ArrayList<>())) {
-            queue.add(route);
-            visited[route] = true;
+        for (int busStop : busStopToRouteIndexMap.getOrDefault(source, new ArrayList<>())) {
+            queue.offer(busStop);
+            visited[busStop] = true;
         }
-        int busCount = 1;
+        int hops = 1;
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size-- > 0) {
+            int queueSize = queue.size();
+            while (queueSize-- > 0) {
                 int route = queue.poll();
-                // Iterate over the stops in the current route.
-                for (int stop : routes[route]) {
-                    // Return the current count if the target is found.
-                    if (stop == target) {
-                        return busCount;
+                for (int busStop : routes[route]) {
+                    if (busStop == target) {
+                        return hops;
                     }
-                    // Iterate over the next possible routes from the current stop.
-                    for (int nextNode : busRouteToStopIndexMap.getOrDefault(stop, Collections.emptyList())) {
-                        if (!visited[nextNode]) {
-                            queue.add(nextNode);
-                            visited[nextNode] = true;
+                    for (int nextBusStop : busStopToRouteIndexMap.getOrDefault(busStop, Collections.emptyList())) {
+                        if (!visited[nextBusStop]) {
+                            queue.offer(nextBusStop);
+                            visited[nextBusStop] = true;
                         }
                     }
                 }
             }
-            busCount++;
+            hops++;
         }
         return -1;
     }
