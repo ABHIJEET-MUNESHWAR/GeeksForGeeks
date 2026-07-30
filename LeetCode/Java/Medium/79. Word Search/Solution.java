eb01d3ebd99f50007f6698abcf9e8586165eb3a5
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 16:11:06 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/79. Word Search/Solution.java
+++ LeetCode/Java/Medium/79. Word Search/Solution.java
@@ -1,40 +1,24 @@
 class Solution {
-    int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-
-    public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        boolean[][] isVisited = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (wordSearchDFS(board, word, 0, i, j, isVisited)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+public boolean exist(char[][] board, String word) {
+    char[] w = word.toCharArray();
+    for (int y=0; y<board.length; y++) {
+    	for (int x=0; x<board[y].length; x++) {
+    		if (exist(board, y, x, w, 0)) return true;
+    	}
     }
+    return false;
+}
 
-    public boolean wordSearchDFS(char[][] board, String word, int index, int i, int j, boolean[][] isVisited) {
-        int m = board.length;
-        int n = board[0].length;
-        if (index == word.length() - 1) {
-            return true;
-        }
-        isVisited[i][j] = true;
-        for (int[] direction : directions) {
-            int i_ = i + direction[0];
-            int j_ = j + direction[1];
-            if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && !isVisited[i_][j_]
-                    && board[i_][j_] == word.charAt(index + 1)) {
-                if (wordSearchDFS(board, word, index + 1, i_, j_, isVisited)) {
-                    return true;
-                }
-            }
-        }
-        isVisited[i][j] = false;
-        return false;
-    }
+private boolean exist(char[][] board, int y, int x, char[] word, int i) {
+	if (i == word.length) return true;
+	if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+	if (board[y][x] != word[i]) return false;
+	board[y][x] ^= 256;
+	boolean exist = exist(board, y, x+1, word, i+1)
+		|| exist(board, y, x-1, word, i+1)
+		|| exist(board, y+1, x, word, i+1)
+		|| exist(board, y-1, x, word, i+1);
+	board[y][x] ^= 256;
+	return exist;
+}
 }
