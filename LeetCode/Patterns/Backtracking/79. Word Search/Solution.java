22c3d5e0213bdfbfe303b9eb227d4fbfb1003c33
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 16:07:59 +0530

    Add Java Solution to Patterns/Backtracking

--- LeetCode/Patterns/Backtracking/79. Word Search/Solution.java
+++ LeetCode/Patterns/Backtracking/79. Word Search/Solution.java
@@ -1,14 +1,12 @@
 class Solution {
-    int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                boolean[][] isVisited = new boolean[m][n];
-                if (board[i][j] == word.charAt(0)) {
-                    if (wordSearchDFS(board, word, 0, i, j, isVisited)) {
+        int rows = board.length;
+        int cols = board[0].length;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (board[r][c] == word.charAt(0)) {
+                    boolean isFound = dfs(board, r, c, word, 0);
+                    if (isFound) {
                         return true;
                     }
                 }
@@ -17,24 +15,33 @@ class Solution {
         return false;
     }
 
-    public boolean wordSearchDFS(char[][] board, String word, int index, int i, int j, boolean[][] isVisited) {
-        int m = board.length;
-        int n = board[0].length;
-        if (index == word.length() - 1) {
+    private boolean dfs(char[][] board, int r, int c, String word, int wordIndex) {
+        // base case
+        // valid
+        if (word.length() == wordIndex) {
             return true;
         }
-        isVisited[i][j] = true;
-        for (int[] direction : directions) {
-            int i_ = i + direction[0];
-            int j_ = j + direction[1];
-            if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && !isVisited[i_][j_]
-                    && board[i_][j_] == word.charAt(index + 1)) {
-                if (wordSearchDFS(board, word, index + 1, i_, j_, isVisited)) {
-                    return true;
-                }
-            }
+        // out of bound cases
+        int rows = board.length;
+        int cols = board[0].length;
+        if (r < 0 || c < 0 || r >= rows || c >= cols) {
+            return false;
         }
-        isVisited[i][j] = false;
+        // invalid cases
+        if (board[r][c] == ' ' || board[r][c] != word.charAt(wordIndex)) {
+            return false;
+        }
+        // mark as visited
+        char ch = board[r][c];
+        board[r][c] = ' ';
+        if (dfs(board, r + 1, c, word, wordIndex + 1)
+                || dfs(board, r - 1, c, word, wordIndex + 1)
+                || dfs(board, r, c + 1, word, wordIndex + 1)
+                || dfs(board, r, c - 1, word, wordIndex + 1)) {
+            return true;
+        }
+        // backtracking
+        board[r][c] = ch;
         return false;
     }
 }
