e4914e01b4f05fa0700fcf4a74ded36dbefedb43
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 11:42:00 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/3275. Minimum Number of Pushes to Type Word I/Solution.java
+++ LeetCode/Java/Easy/3275. Minimum Number of Pushes to Type Word I/Solution.java
@@ -1,18 +1,10 @@
 class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
-        int result = 0;
-        int[] letterToFrequencyMap = new int[26];
+        int pushCount = 0;
         for (int i = 0; i < n; i++) {
-            letterToFrequencyMap[word.charAt(i) - 'a'] = 1;
+            pushCount += (i / 8) + 1;
         }
-        Integer[] mpInteger = Arrays.stream(letterToFrequencyMap).boxed().toArray(Integer[]::new);
-        Arrays.sort(mpInteger, Comparator.reverseOrder());
-        for (int i = 0; i < 26; i++) {
-            int frequency = mpInteger[i];
-            int press = (i / 8) + 1;
-            result += frequency * press;
-        }
-        return result;
+        return pushCount;
     }
 }
