879dda92fed9fa7b2a3df2143753bb8f21fa47ad
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 16:32:29 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/2121. Find if Path Exists in Graph/Solution.java
+++ LeetCode/Java/Easy/2121. Find if Path Exists in Graph/Solution.java
@@ -1,35 +1,29 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        // Create Adjacency List
-        List<List<Integer>> adjacencyList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adjacencyList.add(new ArrayList<>());
-        }
-        // Fill values
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adjacencyList.get(u).add(v);
-            adjacencyList.get(v).add(u);
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.computeIfAbsent(u, key -> new ArrayList<>()).add(v);
+            adj.computeIfAbsent(v, key -> new ArrayList<>()).add(u);
         }
         boolean[] isVisited = new boolean[n];
-        return dfs(adjacencyList, source, destination, isVisited);
-    }
-
-    private boolean dfs(List<List<Integer>> adjacencyList, int currentNode, int destination,
-            boolean[] isVisited) {
-        isVisited[currentNode] = true;
-        if (currentNode == destination) {
+        if (dfs(adj, source, destination, isVisited)) {
             return true;
         }
-        // Visit all neighbours of current node
-        for (Integer neighbor : adjacencyList.get(currentNode)) {
-            if (!isVisited[neighbor]) {
-                isVisited[neighbor] = true;
-                boolean isFound = dfs(adjacencyList, neighbor, destination, isVisited);
-                if (isFound) {
-                    return true;
-                }
+        return false;
+    }
+
+    public boolean dfs(Map<Integer, List<Integer>> adj, int u, int destination, boolean[] isVisited) {
+        if (u == destination) {
+            return true;
+        }
+        if (!isVisited[u]) {
+            isVisited[u] = true;
+        }
+        for (Integer v : adj.getOrDefault(u, new ArrayList<>())) {
+            if (!isVisited[v] && dfs(adj, v, destination, isVisited)) {
+                return true;
             }
         }
         return false;
