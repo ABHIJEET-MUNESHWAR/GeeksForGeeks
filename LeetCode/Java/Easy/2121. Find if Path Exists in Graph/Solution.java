74e5abc27f751a62a12012ecd6943ffd5e1e5ae9
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 16:48:15 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/2121. Find if Path Exists in Graph/Solution.java
+++ LeetCode/Java/Easy/2121. Find if Path Exists in Graph/Solution.java
@@ -18,7 +18,9 @@ class Solution {
         if (u == destination) {
             return true;
         }
-        isVisited[u] = true;
+        if (!isVisited[u]) {
+            isVisited[u] = true;
+        }
         for (Integer v : adj.getOrDefault(u, new ArrayList<>())) {
             if (!isVisited[v] && dfs(adj, v, destination, isVisited)) {
                 return true;
