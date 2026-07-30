744ed2a61ecb04a7a18bd1c2df56b41ba5600951
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 15:18:20 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/417. Pacific Atlantic Water Flow/Solution.java
+++ LeetCode/Java/Medium/417. Pacific Atlantic Water Flow/Solution.java
@@ -6,8 +6,6 @@ class Solution {
         int n = heights[0].length;
         boolean[][] pacificBoolean = new boolean[m][n];
         boolean[][] atlanticBoolean = new boolean[m][n];
-        boolean[][] pacificVisited = new boolean[m][n];
-        boolean[][] atlanticVisited = new boolean[m][n];
         for (int i = 0; i < m; i++) {
             pacificBoolean[i][0] = true;
             atlanticBoolean[i][n - 1] = true;
@@ -19,14 +17,14 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (pacificBoolean[i][j]) {
-                    pacificAtlanticDFS(heights, pacificBoolean, pacificVisited, i, j);
+                    pacificAtlanticDFS(heights, pacificBoolean, i, j);
                 }
             }
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (atlanticBoolean[i][j]) {
-                    pacificAtlanticDFS(heights, atlanticBoolean, atlanticVisited, i, j);
+                    pacificAtlanticDFS(heights, atlanticBoolean, i, j);
                 }
             }
         }
@@ -41,11 +39,7 @@ class Solution {
         return resultList;
     }
 
-    public void pacificAtlanticDFS(int[][] heights, boolean[][] oceanBoolean, boolean[][] visited, int i, int j) {
-        if (visited[i][j]) {
-            return;
-        }
-        visited[i][j] = true;
+    public void pacificAtlanticDFS(int[][] heights, boolean[][] oceanBoolean, int i, int j) {
         int m = heights.length;
         int n = heights[0].length;
         for (int[] direction : directions) {
@@ -53,7 +47,7 @@ class Solution {
             int j_ = j + direction[1];
             if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && !oceanBoolean[i_][j_] && heights[i_][j_] >= heights[i][j]) {
                 oceanBoolean[i_][j_] = true;
-                pacificAtlanticDFS(heights, oceanBoolean, visited, i_, j_);
+                pacificAtlanticDFS(heights, oceanBoolean, i_, j_);
             }
         }
     }
