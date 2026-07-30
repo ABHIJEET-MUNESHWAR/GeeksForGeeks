9491abcd5e56b68c2d81adb6652bee255172d890
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 16:20:04 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/2121. Find if Path Exists in Graph/Solution.java
+++ LeetCode/Java/Easy/2121. Find if Path Exists in Graph/Solution.java
@@ -1,43 +1,34 @@
-class DisjointSetUnion {
-
-  private int[] parent;
-  private int n;
-
-  public DisjointSetUnion(int n) {
-    this.n = n;
-    this.parent = new int[n];
-    for (int i = 0; i < n; i++) {
-      this.parent[i] = i;
-    }
-  }
-
-  public int find(int u) {
-    int x = u;
-    while (x != this.parent[x]) {
-      x = this.parent[x];
-    }
-    this.parent[u] = x;
-    return x;
-  }
-
-  public void union(int u, int v) {
-    if (u != v) {
-      int x = find(u);
-      int y = find(v);
-      parent[x] = y;
-    }
-  }
-
-  public boolean areConnected(int u, int v) {
-    return find(u) == find(v);
-  }
-}
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        DisjointSetUnion disjointSetUnion = new DisjointSetUnion(n);
-        for (int[] edge : edges) {
-            disjointSetUnion.union(edge[0], edge[1]);
+
+    boolean[] visited = new boolean[n];
+    HashSet<Integer>[] graph = new HashSet[n];
+    for (int i = 0; i < n; i++) {
+      graph[i] = new HashSet<>();
+    }
+    for (int[] edge : edges) {
+      graph[edge[0]].add(edge[1]);
+      graph[edge[1]].add(edge[0]);
+    }
+    if (graph[source].contains(destination)) {
+      return true;
+    }
+    Queue<Integer> queue = new PriorityQueue<>();
+    visited[source] = true;
+    queue.add(source);
+    int current;
+    while (!queue.isEmpty()) {
+      current = queue.poll();
+      if (current == destination) {
+        return true;
+      }
+      for (int neighbour : graph[current]) {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          queue.add(neighbour);
         }
-        return disjointSetUnion.areConnected(source, destination);        
+      }
+    }
+    return false;        
     }
 }
