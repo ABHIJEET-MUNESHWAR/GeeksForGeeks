27d2cc80b403e5f3afda424ab876db2139c4ee63
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-30 20:14:05 +0530

    Add Java Solution to Patterns/Trees

--- LeetCode/Patterns/Trees/2421. Number of Good Paths/Solution.java
+++ LeetCode/Patterns/Trees/2421. Number of Good Paths/Solution.java
@@ -1,32 +1,84 @@
-class Solution {
-    int goodPaths = 0;
-
-    public int numberOfGoodPaths(int[] vals, int[][] edges) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        int n = vals.length;
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            adj.computeIfAbsent(u, key -> new ArrayList<>()).add(v);
-            adj.computeIfAbsent(v, key -> new ArrayList<>()).add(u);
-        }
-        for (int key : adj.keySet()) {
-            boolean[] isVisited = new boolean[n];
-            isGoodPathDFS(adj, vals, isVisited, key, key);
-        }
-        return goodPaths / 2 + n;
+public int numberOfGoodPaths(int[] vals, int[][] edges) {
+    int N = vals.length;
+    ArrayList<Integer>[] adj = new ArrayList[N];
+    TreeMap<Integer, ArrayList<Integer>> sameValues = new TreeMap<>();
+    int ans = 0;
+    
+    for(int i=0; i<N; i++) {
+        adj[i] = new ArrayList<>();
+        
+        if(!sameValues.containsKey(vals[i])) sameValues.put(vals[i], new ArrayList<>());
+        sameValues.get(vals[i]).add(i);
     }
-
-    public void isGoodPathDFS(Map<Integer, List<Integer>> adj, int[] vals, boolean[] isVisited, int source,
-            int current) {
-        if (source != current && vals[source] == vals[current]) {
-            goodPaths++;
+    
+    for(int[] e : edges) {
+        int u = e[0];
+        int v = e[1];
+        
+        if(vals[u] >= vals[v]) {
+            adj[u].add(v);
+        } else {
+            adj[v].add(u);
         }
-        isVisited[current] = true;
-        for (int v : adj.getOrDefault(current, Collections.emptyList())) {
-            if (!isVisited[v] && vals[source] >= vals[v]) {
-                isGoodPathDFS(adj, vals, isVisited, source, v);
+    }
+    
+    UF uf = new UF(N);
+    
+    for(int val : sameValues.entrySet()) {
+        for(int u : sameValues.get(val)) {
+            for(int v : adj[u]) {
+                uf.union(u, v);
             }
         }
+        
+        HashMap<Integer, Integer> group = new HashMap<>();
+        
+        for(int u : sameValues.get(val)) {
+            group.put(uf.find(u), group.getOrDefault(uf.find(u), 0) + 1);
+        }
+        
+        ans += sameValues.get(val).size();
+        
+        for(int key : group.keySet()) {
+            int size = group.get(key);
+            ans += size * (size-1) / 2;
+        }
+    }
+    
+    return ans;
+    
+}
+
+class UF {
+
+    int[] parent;
+
+    public UF(int len) {
+        parent = new int[len];
+        Arrays.fill(parent, -1);
+    }
+
+    public int find(int a) {
+        if(parent[a] >= 0) {
+            return parent[a] = find(parent[a]);    
+        }
+        return a;
+    }
+
+    public boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if(pa == pb) return false;
+
+        if(parent[pa] <= parent[pb]) {
+            parent[pa] += parent[pb];
+            parent[pb] = pa;
+        } else {
+            parent[pb] += parent[pa];
+            parent[pa] = pb;
+        }
+        
+        return true;
     }
 }
