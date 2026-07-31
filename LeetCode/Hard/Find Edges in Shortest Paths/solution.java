a5a991c613b21d17cccf6df743c7fe4f3986e778
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-31 12:52:49 +0530

    [Syncode] LeetCode/Hard/Find Edges in Shortest Paths

---
+++ LeetCode/Hard/Find Edges in Shortest Paths/solution.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean[] findAnswer(int n, int[][] edges) {
+        int totalEdges = edges.length;
+        Map<Integer, List<int[]>> adjacencyMap = new HashMap<>();
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int weight = edge[2];
+            adjacencyMap.computeIfAbsent(u, key -> new ArrayList<>()).add(new int[] { v, weight });
+            adjacencyMap.computeIfAbsent(v, key -> new ArrayList<>()).add(new int[] { u, weight });
+        }
+        long[] sourceMinDistance = dijkstra(n, adjacencyMap, 0);
+        long[] destinationMinDistance = dijkstra(n, adjacencyMap, n - 1);
+        boolean[] answer = new boolean[totalEdges];
+        for (int i = 0; i < totalEdges; i++) {
+            int[] edge = edges[i];
+            int u = edge[0];
+            int v = edge[1];
+            int weight = edge[2];
+            boolean direction1 = sourceMinDistance[u] != Long.MAX_VALUE && destinationMinDistance[v] != Long.MAX_VALUE
+                    && ((sourceMinDistance[u] + weight + destinationMinDistance[v]) == sourceMinDistance[n - 1]);
+            boolean direction2 = sourceMinDistance[v] != Long.MAX_VALUE && destinationMinDistance[u] != Long.MAX_VALUE
+                    && ((sourceMinDistance[v] + weight + destinationMinDistance[u]) == sourceMinDistance[n - 1]);
+            if (direction1 || direction2) {
+                answer[i] = true;
+            }
+        }
+        return answer;
+    }
+
+    public long[] dijkstra(int n, Map<Integer, List<int[]>> adjacencyMap, int source) {
+        long[] minDistance = new long[n];
+        Arrays.fill(minDistance, Long.MAX_VALUE);
+        minDistance[source] = 0;
+        PriorityQueue<long[]> minHeap = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
+        minHeap.offer(new long[] { 0, source });
+        while (!minHeap.isEmpty()) {
+            long[] current = minHeap.poll();
+            long currentWeight = current[0];
+            int currentNode = (int) current[1];
+            if (currentWeight > minDistance[currentNode]) {
+                continue;
+            }
+            for (int[] v : adjacencyMap.getOrDefault(currentNode, Collections.emptyList())) {
+                int nextNode = v[0];
+                int nextWeight = v[1];
+                if (minDistance[nextNode] > (minDistance[currentNode] + nextWeight)) {
+                    minDistance[nextNode] = (minDistance[currentNode] + nextWeight);
+                    minHeap.offer(new long[] { minDistance[nextNode], nextNode });
+                }
+            }
+        }
+        return minDistance;
+    }
+}
--- README.md
+++ README.md
@@ -20,3 +20,4 @@
 | 18 | [Number of Good Paths](./LeetCode/Hard/Number%20of%20Good%20Paths) | [LeetCode](https://leetcode.com/problems/number-of-good-paths/) | Hard | 30 Jul 2026 | 08:14 pm |
 | 19 | [Shortest Path in Binary Matrix](./LeetCode/Medium/Shortest%20Path%20in%20Binary%20Matrix) | [LeetCode](https://leetcode.com/problems/shortest-path-in-binary-matrix/) | Medium | 31 Jul 2026 | 11:19 am |
 | 20 | [Minimum Number of Pushes to Type Word II](./LeetCode/Medium/Minimum%20Number%20of%20Pushes%20to%20Type%20Word%20II) | [LeetCode](https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/) | Medium | 31 Jul 2026 | 12:06 pm |
+| 21 | [Find Edges in Shortest Paths](./LeetCode/Hard/Find%20Edges%20in%20Shortest%20Paths) | [LeetCode](https://leetcode.com/problems/find-edges-in-shortest-paths/) | Hard | 31 Jul 2026 | 12:52 pm |
