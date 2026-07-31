69bbfb381f51be467771e397c389385412ba305a
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-31 11:09:44 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/1171. Shortest Path in Binary Matrix/Solution.java
+++ LeetCode/Patterns/Arrays/1171. Shortest Path in Binary Matrix/Solution.java
@@ -1,38 +1,45 @@
 class Solution {
-    public int shortestPathBinaryMatrix(int[][] grid) {
+    class Pair {
+        int x;
+        int y;
+        int distance;
 
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        public Pair(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+    }
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        boolean[][] visited = new boolean[n][n];
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
-        int answer = 0;
-        int row = grid.length;
-        int col = grid[0].length;
-        boolean[][] visited = new boolean[row][col];
-        if (grid[0][0] == 1 || grid[row - 1][col - 1] == 1) {
-            return -1;
-        }
-        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }, { -1, 1 }, { 1, 1 }, { -1, -1 }, { 1, -1 } };
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] { 0, 0 });
+        Pair pair = new Pair(0, 0, 1);
         visited[0][0] = true;
-        while (!queue.isEmpty()) {
-            answer++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] currentPosition = queue.poll();
-                if (currentPosition[0] == row - 1 && currentPosition[1] == col - 1) {
-                    return answer;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((Pair p1, Pair p2) -> p1.distance - p2.distance);
+        pq.add(pair);
+        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }, { -1, 1 }, { 1, 1 }, { -1, -1 }, { 1, -1 } };
+        while (!pq.isEmpty()) {
+            Pair p = pq.poll();
+            int x = p.x;
+            int y = p.y;
+            int distance = p.distance;
+            if (x == n - 1 && y == n - 1) {
+                return distance;
+            }
+            for (int[] direction : directions) {
+                int nextX = x + direction[0];
+                int nextY = y + direction[1];
+                if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= n || visited[nextX][nextY]
+                        || grid[nextX][nextY] == 1) {
+                    continue;
                 }
-                for (int[] direction : directions) {
-                    int nextX = currentPosition[0] + direction[0];
-                    int nextY = currentPosition[1] + direction[1];
-                    if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col || visited[nextX][nextY]
-                            || grid[nextX][nextY] == 1) {
-                        continue;
-                    }
-                    visited[nextX][nextY] = true;
-                    queue.add(new int[] { nextX, nextY });
-                }
+                visited[nextX][nextY] = true;
+                Pair nextPair = new Pair(nextX, nextY, distance + 1);
+                pq.add(nextPair);
             }
         }
         return -1;
