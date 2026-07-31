6236f6197857a8a625721e115608f0f0962bc765
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-31 11:19:51 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/1171. Shortest Path in Binary Matrix/Solution.java
+++ LeetCode/Java/Medium/1171. Shortest Path in Binary Matrix/Solution.java
@@ -1,46 +1,34 @@
 class Solution {
-    class Pair {
-        int x;
-        int y;
-        int distance;
-
-        public Pair(int x, int y, int distance) {
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
-        }
-    }
-
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-        boolean[][] visited = new boolean[n][n];
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
+        if (n == 0 || grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
-        Pair pair = new Pair(0, 0, 1);
-        visited[0][0] = true;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((Pair p1, Pair p2) -> p1.distance - p2.distance);
-        pq.add(pair);
-        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }, { -1, 1 }, { 1, 1 }, { -1, -1 }, { 1, -1 } };
-        while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-            int x = p.x;
-            int y = p.y;
-            int distance = p.distance;
-            if (x == n - 1 && y == n - 1) {
-                return distance;
-            }
-            for (int[] direction : directions) {
-                int nextX = x + direction[0];
-                int nextY = y + direction[1];
-                if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= n || visited[nextX][nextY]
-                        || grid[nextX][nextY] == 1) {
-                    continue;
+        boolean[][] isVisited = new boolean[n][n];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] { 0, 0 });
+        int path = 1;
+        int[][] directions = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 }, { 1, 1 }, { -1, -1 }, { 1, -1 }, { -1, 1 } };
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+            while (queueSize-- > 0) {
+                int[] currentPosition = queue.poll();
+                int currentX = currentPosition[0];
+                int currentY = currentPosition[1];
+                if ((currentX == (n - 1)) && (currentY == (n - 1))) {
+                    return path;
                 }
-                visited[nextX][nextY] = true;
-                Pair nextPair = new Pair(nextX, nextY, distance + 1);
-                pq.add(nextPair);
+                for (int[] direction : directions) {
+                    int nextX = currentX + direction[0];
+                    int nextY = currentY + direction[1];
+                    if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < n && grid[nextX][nextY] == 0
+                            && !isVisited[nextX][nextY]) {
+                        isVisited[nextX][nextY] = true;
+                        queue.offer(new int[] { nextX, nextY });
+                    }
+                }
             }
+            path++;
         }
         return -1;
     }
