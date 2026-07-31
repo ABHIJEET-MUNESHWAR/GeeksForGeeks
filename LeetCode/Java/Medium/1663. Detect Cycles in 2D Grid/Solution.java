045319c88b6877fa8fe8f14cf1962078bb3ed665
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-07-31 19:15:07 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/1663. Detect Cycles in 2D Grid/Solution.java
+++ LeetCode/Java/Medium/1663. Detect Cycles in 2D Grid/Solution.java
@@ -1,35 +1,45 @@
 class Solution {
-    int[][] directions = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+    int[] dx = { 1, -1, 0, 0 };
+    int[] dy = { 0, 0, 1, -1 };
 
     public boolean containsCycle(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] isVisited = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!isVisited[i][j] && dfs(grid, i, j, -1, -1, isVisited)) {
-                    return true;
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean[][] vis = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j]) {
+                    if (dfs(i, j, -1, -1, grid, vis))
+                        return true;
                 }
             }
         }
         return false;
     }
 
-    public boolean dfs(char[][] grid, int i, int j, int prevI, int prevJ, boolean[][] isVisited) {
-        if (isVisited[i][j]) {
-            return true;
-        }
-        isVisited[i][j] = true;
+    public boolean dfs(int x, int y, int px, int py, char[][] grid, boolean[][] vis) {
+        vis[x][y] = true;
 
-        int m = grid.length;
-        int n = grid[0].length;
-        for (int[] direction : directions) {
-            int i_ = i + direction[0];
-            int j_ = j + direction[1];
-            if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && grid[i_][j_] == grid[i][j] && (i_ != prevI && j_ != prevJ)) {
-                if (dfs(grid, i_, j_, i, j, isVisited)) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+                continue;
+            if (grid[nx][ny] != grid[x][y])
+                continue;
+
+            if (vis[nx][ny] && !(nx == px && ny == py))
+                return true;
+
+            if (!vis[nx][ny]) {
+                if (dfs(nx, ny, x, y, grid, vis))
                     return true;
-                }
             }
         }
         return false;
