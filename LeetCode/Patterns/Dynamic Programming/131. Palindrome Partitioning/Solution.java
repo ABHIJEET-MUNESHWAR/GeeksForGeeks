4b43b81f1fa3f08bc34d7368231debd80e17d6c9
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-01 18:43:03 +0530

    Add Java Solution to Patterns/Dynamic Programming

---
+++ LeetCode/Patterns/Dynamic Programming/131. Palindrome Partitioning/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<String>(), s, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, ArrayList<String> tempList, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = start; i < s.length(); i++) {
+                if (isPallindrome(s, start, i)) {
+                    tempList.add(s.substring(start, i + 1));
+                    backtrack(result, tempList, s, i + 1);
+                    tempList.remove(tempList.size() - 1);
+                }
+            }
+        }
+    }
+
+    private boolean isPallindrome(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low++) != s.charAt(high--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
