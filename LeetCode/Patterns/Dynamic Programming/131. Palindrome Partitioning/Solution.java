54e12b0d965e8d25f7d4ce8c50d6d7e4ec82e66e
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-01 19:24:14 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/131. Palindrome Partitioning/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/131. Palindrome Partitioning/Solution.java
@@ -1,29 +1,33 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<String>(), s, 0);
-        return result;
+        List<List<String>> resultList = new ArrayList<>();
+        List<String> currentList = new ArrayList<>();
+        backtrack(s, resultList, currentList, 0);
+        return resultList;
     }
 
-    private void backtrack(List<List<String>> result, ArrayList<String> tempList, String s, int start) {
-        if (start == s.length()) {
-            result.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = start; i < s.length(); i++) {
-                if (isPallindrome(s, start, i)) {
-                    tempList.add(s.substring(start, i + 1));
-                    backtrack(result, tempList, s, i + 1);
-                    tempList.remove(tempList.size() - 1);
-                }
+    public void backtrack(String s, List<List<String>> resultList, List<String> currentList, int index) {
+        if (index == s.length()) {
+            resultList.add(new ArrayList<>(currentList));
+            return;
+        }
+        int n = s.length();
+        for (int i = index; i < n; i++) {
+            if (isPalindrome(s, index, i)) {
+                currentList.add(s.substring(index, i + 1));
+                backtrack(s, resultList, currentList, i + 1);
+                currentList.remove(currentList.size() - 1);
             }
         }
     }
 
-    private boolean isPallindrome(String s, int low, int high) {
-        while (low < high) {
-            if (s.charAt(low++) != s.charAt(high--)) {
+    public boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
+            i++;
+            j--;
         }
         return true;
     }
