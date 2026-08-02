4f892385c1bc6eedc961db6b6dc090b6fcae4e2f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-02 18:46:13 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

---
+++ LeetCode/Patterns/Hash Map & Hashing/49. Group Anagrams/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            map.get(sortedWord).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
