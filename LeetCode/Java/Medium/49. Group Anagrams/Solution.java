f461a40d750a757a5b3b3a83fcf597f06e481704
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-02 19:49:51 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/49. Group Anagrams/Solution.java
+++ LeetCode/Java/Medium/49. Group Anagrams/Solution.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> wordToAnagramsMap = new HashMap<>();
         for (String str : strs) {
             char[] chars = str.toCharArray();
             Arrays.sort(chars);
-            String sortedWord = new String(chars);
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+            String sortedCharsString = new String(chars);
+            if (!wordToAnagramsMap.containsKey(sortedCharsString)) {
+                wordToAnagramsMap.put(sortedCharsString, new ArrayList<>());
             }
-            map.get(sortedWord).add(str);
+            wordToAnagramsMap.get(sortedCharsString).add(str);
         }
-        return new ArrayList<>(map.values());
+        return new ArrayList<>(wordToAnagramsMap.values());
     }
 }
