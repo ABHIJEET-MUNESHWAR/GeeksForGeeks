97cb5a3641ba05403c91441ad683154914089bb2
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-02 19:56:09 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/141. Linked List Cycle/Solution.java
+++ LeetCode/Patterns/Two Pointers/141. Linked List Cycle/Solution.java
@@ -10,18 +10,15 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {        
-        if (head == null) {
-          return false;
-        }
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-          slow = slow.next;
-          fast = fast.next.next;
-          if (slow == fast) {
-            return true;
-          }
+    public boolean hasCycle(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+        while (slowPointer != null && fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            if (slowPointer == fastPointer) {
+                return true;
+            }
         }
         return false;
     }
