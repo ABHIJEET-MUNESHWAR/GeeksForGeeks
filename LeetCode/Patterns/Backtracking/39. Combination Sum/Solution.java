2545101846360f7f5df9706989ac0f56f6b36bfa
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-05 22:28:23 +0530

    Add Java Solution to Patterns/Backtracking

--- LeetCode/Patterns/Backtracking/39. Combination Sum/Solution.java
+++ LeetCode/Patterns/Backtracking/39. Combination Sum/Solution.java
@@ -1,22 +1,26 @@
 class Solution {
+    List<List<Integer>> resultList = new ArrayList<>();
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> currentList = new ArrayList<>();
+        int n = candidates.length;
         Arrays.sort(candidates);
-        backtrack(result, new ArrayList<Integer>(), candidates, target, 0);
-        return result;
+        backTrack(candidates, target, n, 0, currentList);
+        return resultList;
     }
 
-    private void backtrack(List<List<Integer>> result, ArrayList<Integer> tempList, int[] nums, int remain, int start) {
-        if (remain == 0) {
-            result.add(new ArrayList<>(tempList));
-        } else if (remain < 0) {
+    public void backTrack(int[] candidates, int target, int n, int index, List<Integer> currentList) {
+        if (target == 0) {
+            resultList.add(new ArrayList<>(currentList));
             return;
-        } else {
-            for (int i = start; i < nums.length; i++) {
-                tempList.add(nums[i]);
-                backtrack(result, tempList, nums, remain - nums[i], i);
-                tempList.remove(tempList.size() - 1);
-            }
+        }
+        if (target < 0 || index == n) {
+            return;
+        }
+        for (int i = index; i < n; i++) {
+            currentList.add(candidates[i]);
+            backTrack(candidates, target - candidates[i], n, i, currentList);
+            currentList.remove(currentList.size() - 1);
         }
     }
 }
