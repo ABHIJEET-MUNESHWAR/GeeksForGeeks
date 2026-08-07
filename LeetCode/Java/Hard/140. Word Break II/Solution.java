c4166e0ffc0f6a28287cfae037a1e014d8f81b18
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-07 21:35:54 +0530

    Add Hard Java Solution from LeetCode

--- LeetCode/Java/Hard/140. Word Break II/Solution.java
+++ LeetCode/Java/Hard/140. Word Break II/Solution.java
@@ -1,33 +1,29 @@
 class Solution {
 
-    Set<String> set = new HashSet<>();
-    Map<String, List<String>> dpMap = new HashMap<>();
-
     public List<String> wordBreak(String s, List<String> wordDict) {
-        set.addAll(wordDict);
-        return solveWordBreakTwo(s);
+        List<String> resultList = new ArrayList<>();
+        String currentList = new String();
+        Set<String> wordDictSet = new HashSet<>();
+        wordDictSet.addAll(wordDict);
+        backTrackWordBreak(s, wordDictSet, currentList, resultList, 0);
+        return resultList;
     }
 
-    private List<String> solveWordBreakTwo(String s) {
-        if (s.isEmpty()) {
-            return Arrays.asList("");
+    public void backTrackWordBreak(String s, Set<String> wordDictSet, String currentList, List<String> resultList,
+            int i) {
+        int n = s.length();
+        if (i == n) {
+            resultList.add(new String(currentList));
+            return;
         }
-        if (dpMap.containsKey(s)) {
-            return dpMap.get(s);
-        }
-        List<String> result = new ArrayList<>();
-        for (int l = 1; l <= s.length(); l++) {
-            String currentWord = s.substring(0, l);
-            if (set.contains(currentWord)) {
-                String remainingWord = s.substring(l);
-                List<String> remainingResult = solveWordBreakTwo(remainingWord);
-                for (String word : remainingResult) {
-                    String toAdd = currentWord + (word.isEmpty() ? "" : " ") + word;
-                    result.add(toAdd);
-                }
+        for (int j = i + 1; j <= n; j++) {
+            String subString = s.substring(i, j);
+            if (wordDictSet.contains(subString)) {
+                String temp = currentList;
+                currentList = currentList.isEmpty() ? subString : currentList + " " + subString;
+                backTrackWordBreak(s, wordDictSet, currentList, resultList, j);
+                currentList = temp;
             }
         }
-        dpMap.put(s, result);
-        return result;
     }
 }
