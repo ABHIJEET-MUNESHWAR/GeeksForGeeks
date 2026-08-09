acb70338093f7894bafdc9dc77fa8b31ac4cca92
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-09 20:29:24 +0530

    Add Java Solution to Patterns/Trees

--- LeetCode/Patterns/Trees/98. Validate Binary Search Tree/Solution.java
+++ LeetCode/Patterns/Trees/98. Validate Binary Search Tree/Solution.java
@@ -16,15 +16,15 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-      }
+    }
 
-      private boolean isValidBST(TreeNode root, long minValue, long maxValue) {
+    private boolean isValidBST(TreeNode root, long minValue, long maxValue) {
         if (root == null) {
-          return true;
+            return true;
         }
         if (root.val <= minValue || root.val >= maxValue) {
-          return false;
+            return false;
         }
         return isValidBST(root.left, minValue, root.val) && isValidBST(root.right, root.val, maxValue);
-      }
+    }
 }
