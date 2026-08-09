7b7a5e95d523a1524a58a9d77d868ae5a14438c7
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-09 19:41:49 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/153. Find Minimum in Rotated Sorted Array/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
