a99d3bf508b47b4059d8ca7f782a5c560461a90f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-09 21:36:58 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/213. House Robber II/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/213. House Robber II/Solution.java
@@ -1,23 +1,30 @@
 class Solution {
+    int[] dp = new int[101];
+
     public int rob(int[] nums) {
-        if (nums.length == 1) {
+        int size = nums.length;
+        if (size == 1) {
             return nums[0];
         }
-        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
-    }
+        int[] dp = new int[size + 1];
+        dp[0] = 0;
 
-    private int rob(int[] nums, int start, int end) {
-        int prevPrev = 0;
-        int prev = 0;
-
-        for (int i = start; i <= end; i++) {
-            int steal = nums[i] + prevPrev;
-            int skip = prev;
-            int temp = Math.max(steal, skip);
-            prevPrev = prev;
-            prev = temp;
+        for (int i = 1; i <= size - 1; i++) {
+            int steal = nums[i - 1] + (((i - 2) >= 0) ? dp[i - 2] : 0);
+            int skip = dp[i - 1];
+            dp[i] = Math.max(steal, skip);
         }
-
-        return prev;
+        int result1 = dp[size - 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        dp = new int[size + 1];
+        for (int i = 2; i <= size; i++) {
+            int steal = nums[i - 1] + dp[i - 2];
+            int skip = dp[i - 1];
+            dp[i] = Math.max(steal, skip);
+        }
+        int result2 = dp[size];
+        return Math.max(result1, result2);
     }
+
 }
