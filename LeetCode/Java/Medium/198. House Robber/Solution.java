bebb9d9852a65e4ca36f2032f330c67e7411fc38
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-09 21:33:15 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/198. House Robber/Solution.java
+++ LeetCode/Java/Medium/198. House Robber/Solution.java
@@ -1,15 +1,16 @@
 class Solution {
-    public int rob(int[] nums) {
-        int n = nums.length;
-        return solve(nums, n, 0);
-    }
 
-    public int solve(int[] nums, int n, int index) {
-        if (index >= n) {
-            return 0;
+    public int rob(int[] nums) {
+        int size = nums.length;
+        int prevPrev = 0;
+        int prev = nums[0];
+        for (int i = 2; i <= size; i++) {
+            int steal = nums[i - 1] + prevPrev;
+            int skip = prev;
+            int temp = Math.max(steal, skip);
+            prevPrev = prev;
+            prev = temp;
         }
-        int rob = nums[index] + solve(nums, n, index + 2);
-        int skip = solve(nums, n, index + 1);
-        return Math.max(rob, skip);
+        return prev;
     }
 }
