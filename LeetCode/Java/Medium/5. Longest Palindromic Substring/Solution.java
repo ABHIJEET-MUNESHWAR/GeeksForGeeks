8d0673ea941ff6a85154539d698d61b489c66e01
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-09 21:39:45 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/5. Longest Palindromic Substring/Solution.java
+++ LeetCode/Java/Medium/5. Longest Palindromic Substring/Solution.java
@@ -1,31 +1,24 @@
 class Solution {
-    int low, maxLength;
-
     public String longestPalindrome(String s) {
-        int size = s.length();
-        int maxLength = 0;
-        int startingIndex = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = i; j < size; j++) {
-                if (isPalindrome(s, i, j)) {
-                    if (maxLength < (j - i + 1)) {
-                        maxLength = j - i + 1;
-                        startingIndex = i;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int maxLength = 1;
+        int startIndex = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i + l <= n; i++) {
+                int j = i + l - 1;
+                if (s.charAt(i) == s.charAt(j) && (dp[i + 1][j - 1] || l == 2)) {
+                    dp[i][j] = true;
+                    if (maxLength < l) {
+                        maxLength = l;
+                        startIndex = i;
                     }
                 }
             }
         }
-        return s.substring(startingIndex, startingIndex + maxLength);
-    }
-
-    private boolean isPalindrome(String s, int i, int j) {
-        if (i >= j) {
-            return true;
-        }
-        if (s.charAt(i) == s.charAt(j)) {
-            return isPalindrome(s, i + 1, j - 1);
-        } else {
-            return false;
-        }
+        return s.substring(startIndex, startIndex + maxLength);
     }
 }
