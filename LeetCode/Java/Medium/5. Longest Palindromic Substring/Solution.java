4c9fdfd5d1c18a6cafc53906e3298bfa111d319f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-09 21:42:15 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/5. Longest Palindromic Substring/Solution.java
+++ LeetCode/Java/Medium/5. Longest Palindromic Substring/Solution.java
@@ -1,24 +1,41 @@
 class Solution {
+
+    int[][] dp = new int[1001][1001];
+
     public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int maxLength = 1;
-        int startIndex = 0;
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
+        int size = s.length();
+        int maxLength = 0;
+        int startingIndex = 0;
+        for (int i = 0; i < 1001; i++) {
+            for (int j = 0; j < 1001; j++) {
+                dp[i][j] = -1;
+            }
         }
-        for (int l = 2; l <= n; l++) {
-            for (int i = 0; i + l <= n; i++) {
-                int j = i + l - 1;
-                if (s.charAt(i) == s.charAt(j) && (dp[i + 1][j - 1] || l == 2)) {
-                    dp[i][j] = true;
-                    if (maxLength < l) {
-                        maxLength = l;
-                        startIndex = i;
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                if (isPalindrome(s, i, j)) {
+                    if (maxLength < (j - i + 1)) {
+                        maxLength = j - i + 1;
+                        startingIndex = i;
                     }
                 }
             }
         }
-        return s.substring(startIndex, startIndex + maxLength);
+        return s.substring(startingIndex, startingIndex + maxLength);
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        if (l >= r) {
+            return true;
+        }
+        if (dp[l][r] != -1) {
+            return dp[l][r] == 1;
+        }
+        if (s.charAt(l) == s.charAt(r)) {
+            dp[l][r] = isPalindrome(s, l + 1, r - 1) ? 1 : 0;
+        } else {
+            dp[l][r] = 0;
+        }
+        return dp[l][r] == 1;
     }
 }
