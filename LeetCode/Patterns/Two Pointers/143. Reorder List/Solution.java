732b185d8f6d782a1b92282762441386070598f0
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-09 20:05:26 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/143. Reorder List/Solution.java
+++ LeetCode/Patterns/Two Pointers/143. Reorder List/Solution.java
@@ -10,36 +10,42 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null)
+
+        if (head == null || head.next == null || head.next.next == null) {
             return;
-
-        // Find the middle of the list
-        ListNode p1 = head;
-        ListNode p2 = head;
-        while (p2.next != null && p2.next.next != null) {
-            p1 = p1.next;
-            p2 = p2.next.next;
         }
 
-        // Reverse the half after middle 1->2->3->4->5->6 to 1->2->3->6->5->4
-        ListNode preMiddle = p1;
-        ListNode preCurrent = p1.next;
-        while (preCurrent.next != null) {
-            ListNode current = preCurrent.next;
-            preCurrent.next = current.next;
-            current.next = preMiddle.next;
-            preMiddle.next = current;
+        // 1. Find Mid of Linked List
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode secondHead = slow.next;
+
+        // 2. Detach the 2 linked lists
+        slow.next = null;
+
+        // 3. Reverse 2nd half of Linked List
+        ListNode previous = null;
+        ListNode current = secondHead;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextNode;
         }
 
-        // Start reorder one by one 1->2->3->6->5->4 to 1->6->2->5->3->4
-        p1 = head;
-        p2 = preMiddle.next;
-        while (p1 != preMiddle) {
-            preMiddle.next = p2.next;
-            p2.next = p1.next;
-            p1.next = p2;
-            p1 = p2.next;
-            p2 = preMiddle.next;
+        // 4. Start reorder 1 by 1
+        ListNode l1 = head;
+        ListNode l2 = previous;
+        while (l2 != null) {
+            ListNode m1 = l1.next;
+            ListNode m2 = l2.next;
+            l1.next = l2;
+            l2.next = m1;
+            l1 = m1;
+            l2 = m2;
         }
     }
 }
