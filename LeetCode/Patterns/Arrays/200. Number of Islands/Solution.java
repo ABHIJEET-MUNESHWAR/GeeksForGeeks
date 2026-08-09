44324d4a7519d459e7f467c8aca8be8541413084
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-09 21:15:45 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/200. Number of Islands/Solution.java
+++ LeetCode/Patterns/Arrays/200. Number of Islands/Solution.java
@@ -1,27 +1,27 @@
 class Solution {
     public int numIslands(char[][] grid) {
-    int count = 0;
-    int rows = grid.length;
-    int cols = grid[0].length;
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        if (grid[i][j] == '1') {
-          DfsMarking(grid, i, j, rows, cols);
-          count++;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1') {
+                    DFSMarking(grid, rows, cols, r, c);
+                    count++;
+                }
+            }
         }
-      }
+        return count;
     }
-    return count;
-  }
 
-  private void DfsMarking(char[][] grid, int i, int j, int rows, int cols) {
-    if (i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j] != '1') {
-      return;
+    public void DFSMarking(char[][] grid, int rows, int cols, int r, int c) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] != '1') {
+            return;
+        }
+        grid[r][c] = '0';
+        DFSMarking(grid, rows, cols, r + 1, c);
+        DFSMarking(grid, rows, cols, r - 1, c);
+        DFSMarking(grid, rows, cols, r, c + 1);
+        DFSMarking(grid, rows, cols, r, c - 1);
     }
-    grid[i][j] = '0';
-    DfsMarking(grid, i - 1, j, rows, cols);
-    DfsMarking(grid, i + 1, j, rows, cols);
-    DfsMarking(grid, i, j - 1, rows, cols);
-    DfsMarking(grid, i, j + 1, rows, cols);
-  }
 }
