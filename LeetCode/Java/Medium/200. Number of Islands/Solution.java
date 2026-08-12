b3b1e1c9a842fbd01f5c76b40e47dda4d686030c
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-12 19:39:01 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/200. Number of Islands/Solution.java
+++ LeetCode/Java/Medium/200. Number of Islands/Solution.java
@@ -1,27 +1,29 @@
 class Solution {
+    int[][] directions = { { 1, 0 }, { 0, 1 }, { 0, -1 }, { -1, 0 } };
+
     public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int count = 0;
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1') {
-                    DFSMarking(grid, rows, cols, r, c);
-                    count++;
+        int totalIslands = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, m, n, i, j);
+                    totalIslands++;
                 }
             }
         }
-        return count;
+        return totalIslands;
     }
 
-    public void DFSMarking(char[][] grid, int rows, int cols, int r, int c) {
-        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] != '1') {
-            return;
+    public void dfs(char[][] grid, int m, int n, int i, int j) {
+        grid[i][j] = '0';
+        for (int[] direction : directions) {
+            int i_ = i + direction[0];
+            int j_ = j + direction[1];
+            if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && grid[i_][j_] == '1') {
+                dfs(grid, m, n, i_, j_);
+            }
         }
-        grid[r][c] = '0';
-        DFSMarking(grid, rows, cols, r + 1, c);
-        DFSMarking(grid, rows, cols, r - 1, c);
-        DFSMarking(grid, rows, cols, r, c + 1);
-        DFSMarking(grid, rows, cols, r, c - 1);
     }
 }
