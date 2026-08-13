ab4c0a50caa3fbacad0f0ab54b9fe2be9b8acc18
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 16:26:05 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/62. Unique Paths/Solution.java
+++ LeetCode/Java/Medium/62. Unique Paths/Solution.java
@@ -2,21 +2,14 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
         }
-        return solve(m, n, 0, 0, dp);
-    }
-
-    private int solve(int m, int n, int i, int j, int[][] dp) {
-        if ((i == m - 1) && (j == n - 1)) {
-            return 1;
-        }
-        if (i < 0 || i >= m || j < 0 || j >= n) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        return dp[i][j] = solve(m, n, i + 1, j, dp) + solve(m, n, i, j + 1, dp);
+        return dp[m - 1][n - 1];
     }
 }
