eb6da103847108c44bfc886d11c233783f1d7a74
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 22:20:44 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/73. Set Matrix Zeroes/Solution.java
+++ LeetCode/Java/Medium/73. Set Matrix Zeroes/Solution.java
@@ -3,18 +3,21 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         boolean isFirstRowZero = false, isFirstColZero = false;
+        // Check first column separately
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
-                isFirstRowZero = true;
-                break;
-            }
-        }
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
                 isFirstColZero = true;
                 break;
             }
         }
+        // Check first row separately
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -23,6 +26,7 @@ class Solution {
                 }
             }
         }
+
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -40,6 +44,5 @@ class Solution {
                 matrix[i][0] = 0;
             }
         }
-
     }
 }
