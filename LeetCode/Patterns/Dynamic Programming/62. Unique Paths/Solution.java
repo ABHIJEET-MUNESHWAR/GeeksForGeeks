ed9616baca8d24b911a22a85b155576f8b126dea
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 16:25:03 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/62. Unique Paths/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/62. Unique Paths/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return solve(m, n, 0, 0);
-    }
-
-    private int solve(int m, int n, int i, int j) {
-        if (i == m - 1 && j == n - 1) {
-            return 1;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
         }
-        if (i < 0 || i >= m || j < 0 || j >= n) {
-            return 0;
-        }
-        return solve(m, n, i + 1, j) + solve(m, n, i, j + 1);
+        return dp[m - 1][n - 1];
     }
 }
