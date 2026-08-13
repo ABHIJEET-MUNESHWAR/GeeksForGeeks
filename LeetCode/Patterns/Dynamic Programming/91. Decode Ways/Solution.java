d657810ff8f3ef3e71db53e9a446e4773eefd08e
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 16:00:36 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/91. Decode Ways/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/91. Decode Ways/Solution.java
@@ -1,24 +1,14 @@
 class Solution {
     public int numDecodings(String s) {
         int size = s.length();
-        for (int i = 0; i < 101; i++) {
-            dp[i] = -1;
-        }
         return solve(s, 0, size);
     }
 
-    int[] dp = new int[101];
-
     private int solve(String s, int index, int size) {
-        if (dp[index] != -1) {
-            return dp[index];
-        }
         if (index == size) {
-            dp[index] = 1;
             return 1; // Found 1 valid split
         }
         if (s.charAt(index) == '0') {
-            dp[index] = 0;
             return 0; // Not possible to split
         }
         int onlyIthChar = solve(s, index + 1, size);
@@ -28,7 +18,6 @@ class Solution {
                 ithAndIthPlusOneChar = solve(s, index + 2, size);
             }
         }
-        dp[index] = ithAndIthPlusOneChar + onlyIthChar;
-        return dp[index];
+        return ithAndIthPlusOneChar + onlyIthChar;
     }
 }
