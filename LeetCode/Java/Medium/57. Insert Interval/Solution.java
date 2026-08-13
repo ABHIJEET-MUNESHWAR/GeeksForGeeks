fbff9ba14191fe5e3295c5819c4b47bdfb7f11bf
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 16:33:31 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/57. Insert Interval/Solution.java
+++ LeetCode/Java/Medium/57. Insert Interval/Solution.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        int totalIntervals = intervals.length;
-        List<int[]> answer = new ArrayList<>();
-        for (int i = 0; i < totalIntervals; i++) {
-            if (newInterval[1] < intervals[i][0]) {
-                answer.add(newInterval);
-                newInterval = intervals[i];
-            } else if (newInterval[0] > intervals[i][1]) {
-                answer.add(intervals[i]);
-            } else {
-                newInterval = new int[] { Math.min(newInterval[0], intervals[i][0]),
-                        Math.max(newInterval[1], intervals[i][1]) };
+        public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList<>();
+        int[] toAdd = newInterval;
+        
+        for (int i = 0; i < intervals.length; i ++) {
+			/*1. No overlap and toAdd appears before current interval, add toAdd to result.*/
+            if (intervals[i][0] > toAdd[1]) {
+                ans.add(toAdd);
+                toAdd = intervals[i];
             }
+            /*2. Has overlap, update the toAdd to the merged interval.*/
+			else if (intervals[i][1] >= toAdd[0])  
+                toAdd = new int[] {Math.min(intervals[i][0], toAdd[0]),
+                                   Math.max(intervals[i][1], toAdd[1]) };
+			/*3. No overlap and toAdd appears after current interval, add current interval to result.*/
+            else ans.add(intervals[i]); 
         }
-        answer.add(newInterval);
-        return answer.toArray(new int[answer.size()][2]);
+        ans.add(toAdd);
+		return ans.toArray(new int[ans.size()][2]);
     }
 }
