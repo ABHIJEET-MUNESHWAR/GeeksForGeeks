f201a4a99aa61b09fcd0a6b0a414aa8d7519ba73
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 16:12:56 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/322. Coin Change/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/322. Coin Change/Solution.java
@@ -3,30 +3,15 @@ class Solution {
 
     public int coinChange(int[] coins, int amount) {
         dp = new int[amount + 1];
-        Arrays.fill(dp, -1);
-        int answer = coinCountDP(coins, amount);
-        return answer == Integer.MAX_VALUE ? -1 : answer;
-    }
-
-    private int coinCountDP(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
-        }
-        if (amount < 0) {
-            return Integer.MAX_VALUE;
-        }
-        if (dp[amount] != -1) {
-            return dp[amount];
-        }
-        int minCoins = Integer.MAX_VALUE;
-        int size = coins.length;
-        for (int i = 0; i < size; i++) {
-            int ans = coinCountDP(coins, amount - coins[i]);
-            if (ans != Integer.MAX_VALUE) {
-                minCoins = Math.min(minCoins, 1 + ans);
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
             }
         }
-        dp[amount] = minCoins;
-        return minCoins;
+        return (dp[amount] == Integer.MAX_VALUE) ? -1 : dp[amount];
     }
 }
