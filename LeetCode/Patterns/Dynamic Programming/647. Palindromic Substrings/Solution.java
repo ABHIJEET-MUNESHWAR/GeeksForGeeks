29607e0da5dfec98d930dec1627d937ecf6f6bfb
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 15:58:40 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/647. Palindromic Substrings/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/647. Palindromic Substrings/Solution.java
@@ -1,25 +1,24 @@
 class Solution {
+
     public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
         int count = 0;
-        int size = s.length();
-        for (int i = 0; i < size; i++) {
-            for (int j = i; j < size; j++) {
-                if (isPalindrome(s, i, j)) {
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i + l <= n; i++) {
+                int j = i + l - 1;
+                if (i == j) {
+                    dp[i][j] = true;
+                } else if ((i + 1) == j) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j]) {
                     count++;
                 }
             }
         }
         return count;
-    }
-
-    private boolean isPalindrome(String s, int left, int right) {
-        if (left >= right) {
-            return true;
-        }
-        if (s.charAt(left) == s.charAt(right)) {
-            return isPalindrome(s, left + 1, right - 1);
-        } else {
-            return false;
-        }
     }
 }
