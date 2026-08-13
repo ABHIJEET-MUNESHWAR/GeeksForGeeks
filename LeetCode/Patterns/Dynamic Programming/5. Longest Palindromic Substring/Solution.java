ae7540f8abb3f64c1bcfa78507e48e85473090b8
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 15:57:37 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/5. Longest Palindromic Substring/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/5. Longest Palindromic Substring/Solution.java
@@ -1,41 +1,24 @@
 class Solution {
-
-    int[][] dp = new int[1001][1001];
-
     public String longestPalindrome(String s) {
-        int size = s.length();
-        int maxLength = 0;
-        int startingIndex = 0;
-        for (int i = 0; i < 1001; i++) {
-            for (int j = 0; j < 1001; j++) {
-                dp[i][j] = -1;
-            }
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int maxLength = 1;
+        int startIndex = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
         }
-        for (int i = 0; i < size; i++) {
-            for (int j = i; j < size; j++) {
-                if (isPalindrome(s, i, j)) {
-                    if (maxLength < (j - i + 1)) {
-                        maxLength = j - i + 1;
-                        startingIndex = i;
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i + l <= n; i++) {
+                int j = i + l - 1;
+                if (s.charAt(i) == s.charAt(j) && (dp[i + 1][j - 1] || l == 2)) {
+                    dp[i][j] = true;
+                    if (maxLength < l) {
+                        maxLength = l;
+                        startIndex = i;
                     }
                 }
             }
         }
-        return s.substring(startingIndex, startingIndex + maxLength);
-    }
-
-    private boolean isPalindrome(String s, int l, int r) {
-        if (l >= r) {
-            return true;
-        }
-        if (dp[l][r] != -1) {
-            return dp[l][r] == 1;
-        }
-        if (s.charAt(l) == s.charAt(r)) {
-            dp[l][r] = isPalindrome(s, l + 1, r - 1) ? 1 : 0;
-        } else {
-            dp[l][r] = 0;
-        }
-        return dp[l][r] == 1;
+        return s.substring(startIndex, startIndex + maxLength);
     }
 }
