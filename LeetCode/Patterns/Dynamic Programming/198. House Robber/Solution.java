40b1c0bef6f7f1d18aa5f904365058e88e7eea6f
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-13 15:43:34 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/198. House Robber/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/198. House Robber/Solution.java
@@ -1,20 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[401];
-        Arrays.fill(dp, -1);
-        return solve(nums, n, 0, dp);
+        return solveTopDown(nums, n, 0);
     }
 
-    public int solve(int[] nums, int n, int index, int[] dp) {
+    public int solveTopDown(int[] nums, int n, int index) {
         if (index >= n) {
             return 0;
         }
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-        int rob = nums[index] + solve(nums, n, index + 2, dp);
-        int skip = solve(nums, n, index + 1, dp);
-        return dp[index] = Math.max(rob, skip);
+        int take = nums[index] + solveTopDown(nums, n, index + 2);
+        int skip = solveTopDown(nums, n, index+1);
+        return Math.max(take, skip);
     }
 }
