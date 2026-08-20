f83348f57d67b3ce9f9bfb958d095a4de7d242f0
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-20 21:45:47 +0530

    Add Java Solution to Patterns/Trees

--- LeetCode/Patterns/Trees/226. Invert Binary Tree/Solution.java
+++ LeetCode/Patterns/Trees/226. Invert Binary Tree/Solution.java
@@ -15,13 +15,13 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-      if (root == null) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
         return root;
-      }
-      TreeNode left = root.left;
-      TreeNode right = root.right;
-      root.left = invertTree(right);
-      root.right = invertTree(left);
-      return root;     
     }
 }
