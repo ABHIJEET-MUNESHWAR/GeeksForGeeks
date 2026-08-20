c670c24b2c304e4ab1fd76950d0ff533135fdf4a
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-20 21:13:49 +0530

    Add Java Solution to Patterns/Backtracking

--- LeetCode/Patterns/Backtracking/37. Sudoku Solver/Solution.java
+++ LeetCode/Patterns/Backtracking/37. Sudoku Solver/Solution.java
@@ -3,45 +3,46 @@ class Solution {
         solve(board);
     }
 
-    private boolean solve(char[][] board) {
+    public boolean isValidSudoku(char[][] board, int row, int col, char d) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == d) {
+                return false;
+            }
+            if (board[i][col] == d) {
+                return false;
+            }
+        }
+        int start_i = (row / 3) * 3;
+        int start_j = (col / 3) * 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + start_i][j + start_j] == d) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean solve(char[][] board) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
                     for (char d = '1'; d <= '9'; d++) {
-                        if (isValid(board, i, j, d)) {
+                        if (isValidSudoku(board, i, j, d)) {
                             board[i][j] = d;
 
                             if (solve(board)) {
                                 return true;
                             }
 
-                            board[i][j] = '.'; // backtrack
+                            board[i][j] = '.';
                         }
                     }
-                    return false; // no valid number found
+                    return false;
                 }
             }
         }
-        return true; // sudoku solved
-    }
-
-    private boolean isValid(char[][] board, int row, int col, char d) {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == d) return false; // check column
-            if (board[row][i] == d) return false; // check row
-        }
-
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-
-        for (int k = 0; k < 3; k++) {
-            for (int l = 0; l < 3; l++) {
-                if (board[startRow + k][startCol + l] == d) {
-                    return false; // check 3x3 box
-                }
-            }
-        }
-
         return true;
     }
 }
