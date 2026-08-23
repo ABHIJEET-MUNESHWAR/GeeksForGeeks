65263af6fc7cc7ef22d68fb3a7ae3586470c022d
Abhijeet Ashok Muneshwar <OPENINGKNOTS@GMAIL.COM>
2026-08-23 15:15:29 +0530

    Add Java Solution to Patterns/Depth-first search

--- LeetCode/Patterns/Depth-first search/207. Course Schedule/Solution.java
+++ LeetCode/Patterns/Depth-first search/207. Course Schedule/Solution.java
@@ -1,34 +1,33 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, ArrayList<Integer>> adj = new HashMap<>();
-        for(int i=0; i<numCourses; i++){
-            adj.put(i, new ArrayList<>());
+    public boolean isCycleDFS(Map<Integer, List<Integer>> adj, int numCourses, boolean[] isVisited,
+            boolean[] inRecursion, int u) {
+        isVisited[u] = true;
+        inRecursion[u] = true;
+        for (int v : adj.getOrDefault(u, new ArrayList<>())) {
+            if (!isVisited[v] && isCycleDFS(adj, numCourses, isVisited, inRecursion, v)) {
+                return true;
+            } else if (inRecursion[v]) {
+                return true;
+            }
         }
-        for(int[] edge: prerequisites){
+        inRecursion[u] = false;
+        return false;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        boolean[] isVisited = new boolean[numCourses];
+        boolean[] inRecursion = new boolean[numCourses];
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for (int[] edge : prerequisites) {
             int u = edge[0];
             int v = edge[1];
-            adj.get(v).add(u);
+            adj.computeIfAbsent(v, key -> new ArrayList<>()).add(u);
         }
-        boolean[] visited = new boolean[numCourses];
-        boolean[] inRecursion = new boolean[numCourses];
-        for(int i=0; i<numCourses; i++){
-            if(!visited[i] && isCycleDFS(adj, visited, inRecursion, i)){
+        for (int i = 0; i < numCourses; i++) {
+            if (!isVisited[i] && isCycleDFS(adj, numCourses, isVisited, inRecursion, i)) {
                 return false;
             }
         }
         return true;
-    }
-    public boolean isCycleDFS(Map<Integer, ArrayList<Integer>> adj, boolean[] visited, boolean[] inRecursion, int u){
-        visited[u]=true;
-        inRecursion[u]=true;
-        for(int v: adj.get(u)){
-            if(!visited[v] && isCycleDFS(adj, visited, inRecursion, v)){
-                return true;
-            } else if(inRecursion[v]){
-                return true;
-            }
-        }
-        inRecursion[u]=false;
-        return false;
     }
 }
